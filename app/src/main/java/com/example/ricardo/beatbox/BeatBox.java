73bde29957baf87ebb72b6cfe70c504dcf31707f
RicardoHdez <jricardohernandezt@gmail.com>
2017-01-31 18:27:24 -0600

    Capitulo 20 Style and Themes

--- app/src/main/java/com/example/ricardo/beatbox/BeatBox.java
+++ app/src/main/java/com/example/ricardo/beatbox/BeatBox.java
@@ -1,7 +1,10 @@
 package com.example.ricardo.beatbox;
 
 import android.content.Context;
+import android.content.res.AssetFileDescriptor;
 import android.content.res.AssetManager;
+import android.media.AudioManager;
+import android.media.SoundPool;
 import android.util.Log;
 
 import java.io.IOException;
@@ -11,12 +14,27 @@ import java.util.List;
 public class BeatBox {
     private static final String TAG = "BeatBox";
     private static final String SOUNDS_FOLDER = "sample_sounds";
+    private static final int MAX_SOUNDS = 5;
     private AssetManager mAssets;
     private List<Sound> mSounds;
+    private SoundPool mSoundPool;
 
     public BeatBox(Context context) {
         mAssets = context.getAssets();
+        mSoundPool = new SoundPool(MAX_SOUNDS, AudioManager.STREAM_MUSIC, 0);
         loadSounds();
+    }
+
+    public void play(Sound sound){
+        Integer soundId = sound.getSoundId();
+        if(soundId == null){
+            return;
+        }
+        mSoundPool.play(soundId, 1.0f, 1.0f, 1, 0, 1.0f);
+    }
+
+    public void realse(){
+        mSoundPool.release();
     }
 
     private void loadSounds() {
@@ -30,13 +48,24 @@ public class BeatBox {
         }
         mSounds = new ArrayList<Sound>();
         for (String filename : soundNames) {
-            String assetPath = SOUNDS_FOLDER + "/" + filename;
-            Sound sound = new Sound(assetPath);
-            mSounds.add(sound);
+            try{
+                String assetPath = SOUNDS_FOLDER + "/" + filename;
+                Sound sound = new Sound(assetPath);
+                load(sound);
+                mSounds.add(sound);
+            }catch (IOException ioe){
+                Log.e(TAG, "No se pudo reproducir el sonido" + filename, ioe);
+            }
         }
     }
 
     public List<Sound> getSounds() {
         return mSounds;
+    }
+
+    private void load(Sound sound)throws IOException{
+        AssetFileDescriptor afd = mAssets.openFd(sound.getAssetPath());
+        int soundId = mSoundPool.load(afd, 1);
+        sound.setSoundId(soundId);
     }
 }
--- app/src/main/java/com/example/ricardo/beatbox/Sound.java
+++ app/src/main/java/com/example/ricardo/beatbox/Sound.java
@@ -3,6 +3,7 @@ package com.example.ricardo.beatbox;
 public class Sound {
     private String mAssetPath;
     private String mName;
+    private Integer mSoundId;
 
     public Sound(String assetPath){
         mAssetPath = assetPath;
@@ -10,10 +11,16 @@ public class Sound {
         String filename = components[components.length - 1];
         mName = filename.replace(".wav", "");
     }
-    public String getmAssetPath(){
+    public String getAssetPath(){
         return mAssetPath;
     }
     public String getName(){
         return mName;
+    }
+    public Integer getSoundId(){
+        return mSoundId;
+    }
+    public void setSoundId(Integer soundId){
+        mSoundId = soundId;
     }
 }
